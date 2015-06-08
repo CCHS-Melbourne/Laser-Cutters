69e30aa2b9c9171cab1451fbaf79fcfb170fcca2
MrMaxP <stuart@slanty.com>
2015-06-08 16:06:06 +0100

    Commented on G & M codes to be deleted, confirmed or modified.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -60,8 +60,8 @@
 // G2  - CW ARC
 // G3  - CCW ARC
 // G4  - Dwell S<seconds> or P<milliseconds>
-// G10 - retract filament according to settings of M207
-// G11 - retract recover filament according to settings of M208
+// G10 - retract filament according to settings of M207 		(****DELETE****) (####REPLACE WITH LASER OFF?####)
+// G11 - retract recover filament according to settings of M208		(****DELETE****)
 // G28 - Home all Axis
 // G90 - Use Absolute Coordinates
 // G91 - Use Relative Coordinates
@@ -86,63 +86,63 @@
 // M31  - Output time since last M109 or SD card start to serial
 // M32  - Select file and start SD print (Can be used when printing from SD card)
 // M42  - Change pin status via gcode Use M42 Px Sy to set pin x to value y, when omitting Px the onboard led will be used.
-// M80  - Turn on Power Supply
-// M81  - Turn off Power Supply
+// M80  - Turn on Power Supply				(****UNUSED****)
+// M81  - Turn off Power Supply				(****UNUSED****)
 // M82  - Set E codes absolute (default)
 // M83  - Set E codes relative while in Absolute Coordinates (G90) mode
 // M84  - Disable steppers until next move,
 //        or use S<seconds> to specify an inactivity timeout, after which the steppers will be disabled.  S0 to disable the timeout.
 // M85  - Set inactivity shutdown timer with parameter S<seconds>. To disable set zero (default)
 // M92  - Set axis_steps_per_unit - same syntax as G92
-// M104 - Set extruder target temp
-// M105 - Read current temp
-// M106 - Fan on
-// M107 - Fan off
-// M109 - Sxxx Wait for extruder current temp to reach target temp. Waits only when heating
-//        Rxxx Wait for extruder current temp to reach target temp. Waits when heating and cooling
+// M104 - Set extruder target temp			(****DELETE****)
+// M105 - Read current temp				(****DELETE****)
+// M106 - Fan on					(****REPURPOSE for extractor fan?****)
+// M107 - Fan off					(****REPURPOSE for extractor fan?****)
+// M109 - Sxxx Wait for extruder current temp to reach target temp. Waits only when heating	(****DELETE****)
+//        Rxxx Wait for extruder current temp to reach target temp. Waits when heating and cooling	(****DELETE****)
 // M114 - Output current position to serial port
 // M115 - Capabilities string
 // M117 - display message
 // M119 - Output Endstop status to serial port
-// M128 - EtoP Open (BariCUDA EtoP = electricity to air pressure transducer by jmil)
-// M129 - EtoP Closed (BariCUDA EtoP = electricity to air pressure transducer by jmil)
-// M140 - Set bed target temp
-// M190 - Sxxx Wait for bed current temp to reach target temp. Waits only when heating
-//        Rxxx Wait for bed current temp to reach target temp. Waits when heating and cooling
-// M200 - Set filament diameter
-// M201 - Set max acceleration in units/s^2 for print moves (M201 X1000 Y1000)
-// M202 - Set max acceleration in units/s^2 for travel moves (M202 X1000 Y1000) Unused in Marlin!!
-// M203 - Set maximum feedrate that your machine can sustain (M203 X200 Y200 Z300 E10000) in mm/sec
-// M204 - Set default acceleration: S normal moves T filament only moves (M204 S3000 T7000) im mm/sec^2  also sets minimum segment time in ms (B20000) to prevent buffer underruns and M20 minimum feedrate
-// M205 -  advanced settings:  minimum travel speed S=while printing T=travel only,  B=minimum segment time X= maximum xy jerk, Z=maximum Z jerk, E=maximum E jerk
+// M128 - EtoP Open (BariCUDA EtoP = electricity to air pressure transducer by jmil)		(****DELETE****)
+// M129 - EtoP Closed (BariCUDA EtoP = electricity to air pressure transducer by jmil)		(****DELETE****)
+// M140 - Set bed target temp									(****DELETE****)
+// M190 - Sxxx Wait for bed current temp to reach target temp. Waits only when heating		(****DELETE****)
+//        Rxxx Wait for bed current temp to reach target temp. Waits when heating and cooling	(****DELETE****)
+// M200 - Set filament diameter									(****DELETE****)
+// M201 - Set max acceleration in units/s^2 for print moves (M201 X1000 Y1000)			(****REALLY?****)
+// M202 - Set max acceleration in units/s^2 for travel moves (M202 X1000 Y1000) Unused in Marlin!!	(****REALLY?****)
+// M203 - Set maximum feedrate that your machine can sustain (M203 X200 Y200 Z300 E10000) in mm/sec	(****DELETE****)
+// M204 - Set default acceleration: S normal moves T filament only moves (M204 S3000 T7000) im mm/sec^2  also sets minimum segment time in ms (B20000) to prevent buffer underruns and M20 minimum feedrate (****REALLY?****)
+// M205 -  advanced settings:  minimum travel speed S=while printing T=travel only,  B=minimum segment time X= maximum xy jerk, Z=maximum Z jerk, E=maximum E jerk		(****REALLY?****)
 // M206 - set additional homeing offset
-// M207 - set retract length S[positive mm] F[feedrate mm/sec] Z[additional zlift/hop]
-// M208 - set recover=unretract length S[positive mm surplus to the M207 S*] F[feedrate mm/sec]
-// M209 - S<1=true/0=false> enable automatic retract detect if the slicer did not support G10/11: every normal extrude-only move will be classified as retract depending on the direction.
-// M218 - set hotend offset (in mm): T<extruder_number> X<offset_on_X> Y<offset_on_Y>
+// M207 - set retract length S[positive mm] F[feedrate mm/sec] Z[additional zlift/hop]		(****DELETE****)
+// M208 - set recover=unretract length S[positive mm surplus to the M207 S*] F[feedrate mm/sec]	(****DELETE****)	
+// M209 - S<1=true/0=false> enable automatic retract detect if the slicer did not support G10/11: every normal extrude-only move will be classified as retract depending on the direction. (****DELETE****)
+// M218 - set hotend offset (in mm): T<extruder_number> X<offset_on_X> Y<offset_on_Y>		(****DELETE****)
 // M220 S<factor in percent>- set speed factor override percentage
-// M221 S<factor in percent>- set extrude factor override percentage
-// M240 - Trigger a camera to take a photograph
-// M250 - Set LCD contrast C<contrast value> (value 0..63)
-// M280 - set servo position absolute. P: servo index, S: angle or microseconds
+// M221 S<factor in percent>- set extrude factor override percentage				(****DELETE****)
+// M240 - Trigger a camera to take a photograph							(****DELETE****)
+// M250 - Set LCD contrast C<contrast value> (value 0..63)					(####REALLY?####)
+// M280 - set servo position absolute. P: servo index, S: angle or microseconds			(****DELETE****)
 // M300 - Play beepsound S<frequency Hz> P<duration ms>
-// M301 - Set PID parameters P I and D
-// M302 - Allow cold extrudes, or set the minimum extrude S<temperature>.
-// M303 - PID relay autotune S<temperature> sets the target temperature. (default target temperature = 150C)
-// M304 - Set bed PID parameters P I and D
+// M301 - Set PID parameters P I and D								(****DELETE****)
+// M302 - Allow cold extrudes, or set the minimum extrude S<temperature>.			(****DELETE****)
+// M303 - PID relay autotune S<temperature> sets the target temperature. (default target temperature = 150C)	(****DELETE****)
+// M304 - Set bed PID parameters P I and D							(****DELETE****)
 // M400 - Finish all moves
 // M500 - stores paramters in EEPROM
 // M501 - reads parameters from EEPROM (if you need reset them after you changed them temporarily).
 // M502 - reverts to the default "factory settings".  You still need to store them in EEPROM afterwards if you want to.
 // M503 - print the current settings (from memory not from eeprom)
 // M540 - Use S[0|1] to enable or disable the stop SD card print on endstop hit (requires ABORT_ON_ENDSTOP_HIT_FEATURE_ENABLED)
-// M600 - Pause for filament change X[pos] Y[pos] Z[relative lift] E[initial retract] L[later retract distance for removal]
+// M600 - Pause for filament change X[pos] Y[pos] Z[relative lift] E[initial retract] L[later retract distance for removal]	(****DELETE****)
 // M649 - 
 // M650 - 
 // M666 - set delta endstop adjustemnt
-// M605 - Set dual x-carriage movement mode: S<mode> [ X<duplication x-offset> R<duplication temp offset> ]
-// M907 - Set digital trimpot motor current using axis codes.
-// M908 - Control digital trimpot directly.
+// M605 - Set dual x-carriage movement mode: S<mode> [ X<duplication x-offset> R<duplication temp offset> ]	(****DELETE****)
+// M907 - Set digital trimpot motor current using axis codes.		(####WHAT DOES THIS DO?####)
+// M908 - Control digital trimpot directly.				(####WHAT DOES THIS DO?####)
 // M350 - Set microstepping mode.
 // M351 - Toggle MS1 MS2 pins directly.
 // M928 - Start SD logging (M928 filename.g) - ended by M29
